41269005f8a4cbc82ca132df292ab788b66890c7
iztaoswif <iztaoswif@gmail.com>
2026-03-24 14:47:25 +0300

    now app can be started in a container

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,12 @@ RUN pip install --no-cache-dir -r prod-requirements.txt
 
 COPY app/ ./app/
 COPY static/ ./static/
+COPY alembic/ ./alembic
+COPY alembic.ini .
+
+COPY entrypoint.sh .
+RUN chmod +x entrypoint.sh
 
 EXPOSE 8000
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+ENTRYPOINT ["./entrypoint.sh"]
--- app/core/redis.py
+++ app/core/redis.py
@@ -1,10 +1,18 @@
 from redis.asyncio import Redis
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 def create_redis_client() -> Redis:
+    host = os.getenv("REDIS_HOST")
+    port = int(os.getenv("REDIS_PORT"))
+    db = int(os.getenv("REDIS_DB"))
+
     return Redis(
-        host=os.getenv("REDIS_HOST", "localhost"),
-        port=int(os.getenv("REDIS_PORT", 6379)),
+        host=host,
+        port=port,
+        db=db,
         decode_responses=True,
         max_connections=30
     )
